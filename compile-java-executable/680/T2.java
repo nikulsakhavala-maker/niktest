c471a0a8c0159b2ea558f330b09a4411f1878980
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 18:01:50 +0000

    Test Case Tags Assigned

--- compile-java-executable/680/T2.java
+++ compile-java-executable/680/T2.java
@@ -61,6 +61,26 @@ public class T2 {
         tg.close();
     }
 
+    public static void function1() {
+        tg.wait("ele_searchamaz318", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_searchamaz318", 1);
+        tg.wait("ele_searchamaz318", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_searchamaz318", "SMARTPHONE");
+        tg.wait("ele_under724", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_under724", 1);
+        tg.wait("ele_samsunggal007", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_samsunggal007", 1);
+        tg.switchToTab(1);
+        tg.wait("ele_inputtgweb414", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_inputtgweb414", 1);
+        tg.wait("ele_inputtgweb098", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_inputtgweb098", 1);
+        tg.wait("ele_inputtgweb171", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_inputtgweb171", 1);
+        tg.wait("ele_buynowbutt618", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_buynowbutt618", 1);
+    }
+
     public static void function2() {
         tg.wait("ele_searchforp943", ComparisonType.IS_VISIBLE, 1);
         tg.click("ele_searchforp943", 1);
