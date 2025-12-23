ce926004b26682e17ac79ba18c012221fd5ac519
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 22:52:31 +0000

    Test Suite Added

--- compile-java-executable/705/BackupTest1.java
+++ compile-java-executable/705/BackupTest1.java
@@ -34,4 +34,15 @@ public class BackupTest1 {
         tg.typeEncrypted("ele_password718", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
         tg.close();
     }
+
+    public static void backuptest1() {
+        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
+        tg.click("ele_emailorpho448", 1);
+        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
+        tg.type("ele_emailorpho448", "Mukund");
+        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password718", 1);
+        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
+        tg.typeEncrypted("ele_password718", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+    }
 }
