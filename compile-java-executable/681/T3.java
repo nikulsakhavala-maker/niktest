4d9790fe182241854a66890857a743e4c6069c85
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 18:48:59 +0000

    Converted Test Case to Test Function

--- compile-java-executable/681/T3.java
+++ compile-java-executable/681/T3.java
@@ -67,4 +67,19 @@ public class T3 {
         tg.click("ele_buynow446", 1);
         tg.close();
     }
+
+    public static void functiont3case1() {
+        tg.wait("ele_searchamaz727", ComparisonType.IS_VISIBLE);
+        tg.click("ele_searchamaz727", 1);
+        tg.wait("ele_searchamaz727", ComparisonType.IS_VISIBLE);
+        tg.type("ele_searchamaz727", "iphone 17 pro max 1 TB");
+        tg.switchToTab(1);
+        tg.wait("ele_iphone17pr813", ComparisonType.IS_VISIBLE);
+        tg.click("ele_iphone17pr813", 1);
+        tg.click("ele_addtocart414", 1);
+        tg.wait("ele_increasequ641", ComparisonType.IS_VISIBLE);
+        tg.click("ele_increasequ641", 1);
+        tg.wait("ele_apopovercl964", ComparisonType.IS_VISIBLE);
+        tg.click("ele_apopovercl964", 1);
+    }
 }
---
+++ compile-java-executable/681/testfunctions/functiont3case2.java
@@ -0,0 +1,26 @@
+class functiont3case2 {
+
+	public static void functiont3case2() {
+		tg.wait("ele_searchforp640", ComparisonType.IS_VISIBLE);
+		tg.click("ele_searchforp640", 1);
+		tg.wait("ele_searchforp640", ComparisonType.IS_VISIBLE);
+		tg.type("ele_searchforp640", "winter jackets for men");
+		tg.wait("ele_winterjack526", ComparisonType.IS_VISIBLE);
+		tg.click("ele_winterjack526", 1);
+		tg.wait("ele_imgtgwebco589", ComparisonType.IS_VISIBLE);
+		tg.click("ele_imgtgwebco589", 1);
+		tg.switchToTab(1);
+		tg.wait("ele_httprukmin914", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_httprukmin914", 1);
+		tg.wait("ele_httpsrukmi562", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_httpsrukmi562", 1);
+		tg.wait("ele_enterdeliv701", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_enterdeliv701", 1);
+		tg.wait("ele_enterdeliv701", ComparisonType.IS_VISIBLE, 1);
+		tg.type("ele_enterdeliv701", "395010");
+		tg.wait("ele_check601", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_check601", 1);
+		tg.wait("ele_buynow446", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_buynow446", 1);
+	}
+}
