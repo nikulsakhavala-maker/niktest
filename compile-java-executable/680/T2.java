7917558d6f4faf01f96a3a13159a42011df96465
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 17:59:55 +0000

    Converted Test Case to Test Function

--- compile-java-executable/680/T2.java
+++ compile-java-executable/680/T2.java
@@ -60,4 +60,18 @@ public class T2 {
         tg.click("ele_addtocart790", 1);
         tg.close();
     }
+
+    public static void function2() {
+        tg.wait("ele_searchforp943", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_searchforp943", 1);
+        tg.wait("ele_searchforp943", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_searchforp943", "shoes");
+        tg.wait("ele_shoesforme629", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_shoesforme629", 1);
+        tg.wait("ele_imgtgwebco190", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_imgtgwebco190", 1);
+        tg.switchToTab(1);
+        tg.wait("ele_addtocart790", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_addtocart790", 1);
+    }
 }
