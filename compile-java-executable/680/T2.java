54cdcfb46e02852a1e5e78043db1ba1ab479a02e
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 18:14:22 +0000

    Converted Test Case to Test Function

--- compile-java-executable/680/T2.java
+++ compile-java-executable/680/T2.java
@@ -41,6 +41,28 @@ public class T2 {
         tg.close();
     }
 
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void t2case2() {
+        tg.openBrowser();
+        tg.wait("ele_firstname728", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_firstname728", 1);
+        tg.wait("ele_firstname728", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_firstname728", "nikul");
+        tg.wait("ele_lastname224", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_lastname224", 1);
+        tg.wait("ele_lastname224", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_lastname224", "sakhavala");
+        tg.wait("ele_textareatgwebcomma046", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_textareatgwebcomma046", 1);
+        tg.wait("ele_textareatgwebcomma046", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_textareatgwebcomma046", "123, radhe park society");
+        tg.wait("ele_emailemail302", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_emailemail302", 1);
+        tg.wait("ele_emailemail302", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_emailemail302", "test@gmail.com");
+        tg.close();
+    }
+
     public static void function1() {
         tg.wait("ele_searchamaz318", ComparisonType.IS_VISIBLE, 1);
         tg.click("ele_searchamaz318", 1);
---
+++ compile-java-executable/680/testfunctions/functiont1.java
@@ -0,0 +1,17 @@
+class functiont1 {
+
+	public static void functiont1() {
+		tg.wait("ele_registerad912", ComparisonType.IS_VISIBLE);
+		tg.click("ele_registerad912", 1);
+		tg.wait("ele_firstname745", ComparisonType.IS_VISIBLE);
+		tg.click("ele_firstname745", 1);
+		tg.wait("ele_firstname745", ComparisonType.IS_VISIBLE);
+		tg.type("ele_firstname745", "fghgfnfgn");
+		tg.wait("ele_lastname165", ComparisonType.IS_VISIBLE);
+		tg.click("ele_lastname165", 1);
+		tg.wait("ele_lastname779", ComparisonType.IS_VISIBLE);
+		tg.click("ele_lastname779", 1);
+		tg.wait("ele_lastname779", ComparisonType.IS_VISIBLE);
+		tg.type("ele_lastname779", "fgngnhnghmhgm");
+	}
+}
