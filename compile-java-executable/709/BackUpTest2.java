34616de3339827f2d8e85fa56d8c9a9a4e0f55a1
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 23:00:26 +0000

    Test Case Added

--- compile-java-executable/709/BackUpTest2.java
+++ compile-java-executable/709/BackUpTest2.java
@@ -36,4 +36,17 @@ public class BackUpTest2 {
         tg.typeEncrypted("ele_password174", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
         tg.close();
     }
+
+    public static void testingback2() {
+        tg.wait("ele_emailorpho148", ComparisonType.IS_VISIBLE);
+        tg.click("ele_emailorpho148", 1);
+        tg.wait("ele_emailorpho148", ComparisonType.IS_VISIBLE);
+        tg.type("ele_emailorpho148", "Mukund");
+        tg.wait("ele_password662", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password662", 1);
+        tg.wait("ele_password174", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password174", 1);
+        tg.wait("ele_password174", ComparisonType.IS_VISIBLE);
+        tg.typeEncrypted("ele_password174", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+    }
 }
