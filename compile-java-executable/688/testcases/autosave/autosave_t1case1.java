724cfa2b580bf810c591538dfc8248592be62da1
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 22:16:35 +0000

    Test Case Updated

---
+++ compile-java-executable/688/testcases/autosave/autosave_t1case1.java
@@ -0,0 +1,50 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class t1case1 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void t1case1() {
+		tg.openBrowser();
+		tg.wait("ele_firstname453", ComparisonType.IS_VISIBLE);
+		tg.click("ele_firstname453", 1);
+		tg.wait("ele_firstname453", ComparisonType.IS_VISIBLE);
+		tg.type("ele_firstname453", "hdhfghfgh");
+		tg.wait("ele_lastname998", ComparisonType.IS_VISIBLE);
+		tg.click("ele_lastname998", 1);
+		tg.wait("ele_lastname381", ComparisonType.IS_VISIBLE);
+		tg.click("ele_lastname381", 1);
+		tg.wait("ele_lastname381", ComparisonType.IS_VISIBLE, 1);
+		tg.type("ele_lastname381", "tyjytjytj");
+		tg.wait("ele_textareatgwebcomma875", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_textareatgwebcomma875", 1);
+		tg.wait("ele_textareatgwebcomma903", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_textareatgwebcomma903", 1);
+		tg.wait("ele_textareatgwebcomma903", ComparisonType.IS_VISIBLE, 1);
+		tg.type("ele_textareatgwebcomma903", "tyjtyjytjytjytj");
+		tg.wait("ele_emailemail442", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_emailemail442", 1);
+		tg.wait("ele_emailemail442", ComparisonType.IS_VISIBLE, 1);
+		tg.type("ele_emailemail442", "tyjtyjytjtyjyt");
+		tg.close();
+	}
+}
