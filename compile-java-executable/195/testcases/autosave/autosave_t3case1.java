858b702098d239093364acfeeca270c8f1208ce7
Nikul <nikul.sakhavala@testgrid.io>
2025-12-24 03:13:52 +0530

    Test Case Updated

---
+++ compile-java-executable/195/testcases/autosave/autosave_t3case1.java
@@ -0,0 +1,38 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class t3case1 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void t3case1() {
+		tg.openBrowser();
+		tg.wait("ele_emailaddre644", ComparisonType.IS_VISIBLE, 1);
+		tg.type("ele_emailaddre644", "test@gmail.com");
+		tg.wait("ele_password544", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_password544", 1);
+		tg.wait("ele_password544", ComparisonType.IS_VISIBLE, 1);
+		tg.typeEncrypted("ele_password544", "qjESCFhPZJrstjjOXDAT9g==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+		tg.wait("ele_login167", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_login167", 1);
+		tg.close();
+	}
+}
