1518ff5f3419cbc1a6b90d80eac03380f0c74d8e
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 18:48:52 +0000

    Converted Test Case to Test Function

---
+++ compile-java-executable/681/testfunctions/functiont3case1.java
@@ -0,0 +1,17 @@
+class functiont3case1 {
+
+	public static void functiont3case1() {
+		tg.wait("ele_searchamaz727", ComparisonType.IS_VISIBLE);
+		tg.click("ele_searchamaz727", 1);
+		tg.wait("ele_searchamaz727", ComparisonType.IS_VISIBLE);
+		tg.type("ele_searchamaz727", "iphone 17 pro max 1 TB");
+		tg.switchToTab(1);
+		tg.wait("ele_iphone17pr813", ComparisonType.IS_VISIBLE);
+		tg.click("ele_iphone17pr813", 1);
+		tg.click("ele_addtocart414", 1);
+		tg.wait("ele_increasequ641", ComparisonType.IS_VISIBLE);
+		tg.click("ele_increasequ641", 1);
+		tg.wait("ele_apopovercl964", ComparisonType.IS_VISIBLE);
+		tg.click("ele_apopovercl964", 1);
+	}
+}
