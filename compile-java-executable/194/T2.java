3e896381fc3b5e8564b9ab0708643a4d70cd281b
Nikul <nikul.sakhavala@testgrid.io>
2025-12-24 03:11:18 +0530

    Test Suite Added

--- compile-java-executable/194/T2.java
+++ compile-java-executable/194/T2.java
@@ -38,4 +38,17 @@ public class T2 {
         tg.type("ele_textarea413", "rthrthtrhtrhtrh");
         tg.close();
     }
+
+    public static void t2function2() {
+        tg.wait("ele_firstname799", ComparisonType.IS_VISIBLE);
+        tg.type("ele_firstname799", "rthrthtrhtrhtrh");
+        tg.wait("ele_lastname385", ComparisonType.IS_VISIBLE);
+        tg.click("ele_lastname385", 1);
+        tg.wait("ele_lastname385", ComparisonType.IS_VISIBLE);
+        tg.type("ele_lastname385", "rthrthtrh");
+        tg.wait("ele_textarea413", ComparisonType.IS_VISIBLE);
+        tg.click("ele_textarea413", 1);
+        tg.wait("ele_textarea413", ComparisonType.IS_VISIBLE);
+        tg.type("ele_textarea413", "rthrthtrhtrhtrh");
+    }
 }
