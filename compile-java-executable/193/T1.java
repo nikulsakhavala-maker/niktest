33cec7c300551548106ca8b8533a5d75e8d3ec90
Nikul <nikul.sakhavala@testgrid.io>
2025-12-24 02:55:57 +0530

    Global Variable Added

--- compile-java-executable/193/T1.java
+++ compile-java-executable/193/T1.java
@@ -46,4 +46,27 @@ public class T1 {
         tg.click("ele_checkbox045", 1);
         tg.close();
     }
+
+    public static void t1function1() {
+        tg.wait("ele_firstname817", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_firstname817", 1);
+        tg.wait("ele_firstname817", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_firstname817", "nikul");
+        tg.wait("ele_lastname973", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_lastname973", 1);
+        tg.wait("ele_lastname973", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_lastname973", "sakhavala");
+        tg.wait("ele_textarea412", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_textarea412", 1);
+        tg.wait("ele_textarea412", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_textarea412", "123, radhe park society");
+        tg.wait("ele_emailemail126", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_emailemail126", 1);
+        tg.wait("ele_emailemail126", ComparisonType.IS_VISIBLE, 1);
+        tg.type("ele_emailemail126", "test@gmail.com");
+        tg.wait("ele_checkbox887", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_checkbox887", 1);
+        tg.wait("ele_checkbox045", ComparisonType.IS_VISIBLE, 1);
+        tg.click("ele_checkbox045", 1);
+    }
 }
