6b9433e6ae27537f449aa7d8a32603220dd7ea7e
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 22:57:56 +0000

    Restored Version from Git Commit - Auto Commit

---
+++ compile-java-executable/708/BackupTest1.java
@@ -0,0 +1,48 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@Listeners(TestListener.class)
+public class BackupTest1 {
+
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void testingbackupcase() {
+        tg.openBrowser();
+        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
+        tg.click("ele_emailorpho448", 1);
+        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
+        tg.type("ele_emailorpho448", "Mukund");
+        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password718", 1);
+        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
+        tg.typeEncrypted("ele_password718", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+        tg.close();
+    }
+
+    public static void backuptest1() {
+        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
+        tg.click("ele_emailorpho448", 1);
+        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
+        tg.type("ele_emailorpho448", "Mukund");
+        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password718", 1);
+        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
+        tg.typeEncrypted("ele_password718", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+    }
+}
--- compile-java-executable/709/BackUpTest2.java
+++ compile-java-executable/709/BackUpTest2.java
@@ -19,19 +19,21 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 @Listeners(TestListener.class)
-public class BackupTest1 {
+public class BackUpTest2 {
 
     @Test(retryAnalyzer = RetryFailedTestCases.class)
-    public void testingbackupcase() {
+    public void testingbackupcase2() {
         tg.openBrowser();
-        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
-        tg.click("ele_emailorpho448", 1);
-        tg.wait("ele_emailorpho448", ComparisonType.IS_VISIBLE);
-        tg.type("ele_emailorpho448", "Mukund");
-        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
-        tg.click("ele_password718", 1);
-        tg.wait("ele_password718", ComparisonType.IS_VISIBLE);
-        tg.typeEncrypted("ele_password718", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
+        tg.wait("ele_emailorpho148", ComparisonType.IS_VISIBLE);
+        tg.click("ele_emailorpho148", 1);
+        tg.wait("ele_emailorpho148", ComparisonType.IS_VISIBLE);
+        tg.type("ele_emailorpho148", "Mukund");
+        tg.wait("ele_password662", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password662", 1);
+        tg.wait("ele_password174", ComparisonType.IS_VISIBLE);
+        tg.click("ele_password174", 1);
+        tg.wait("ele_password174", ComparisonType.IS_VISIBLE);
+        tg.typeEncrypted("ele_password174", "3drhIPuS/gb9ZOl5v6Bp0Q==:MTIzNDU2Nzg5MTAxMTEyMQ==");
         tg.close();
     }
 }
