cd50b6ca55a48a32e5e1ed7d76e9ec4fc4ef6404
TestGrid <nikul.sakhavala@testgrid.io>
2025-12-23 17:50:03 +0000

    Test Case Updated

---
+++ compile-java-executable/680/testcases/autosave/autosave_t2case1.java
@@ -0,0 +1,47 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class t2case1 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void t2case1() {
+		tg.openBrowser();
+		tg.wait("ele_searchamaz318", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_searchamaz318", 1);
+		tg.wait("ele_searchamaz318", ComparisonType.IS_VISIBLE, 1);
+		tg.type("ele_searchamaz318", "SMARTPHONE");
+		tg.wait("ele_under724", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_under724", 1);
+		tg.wait("ele_samsunggal007", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_samsunggal007", 1);
+		tg.switchToTab(1);
+		tg.wait("ele_inputtgweb414", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_inputtgweb414", 1);
+		tg.wait("ele_inputtgweb098", ComparisonType.IS_VISIBLE, 1);
+		tg.click("ele_inputtgweb098", 1);
+		tg.wait("ele_inputtgweb171", ComparisonType.IS_VISIBLE);
+		tg.click("ele_inputtgweb171", 1);
+		tg.wait("ele_buynowbutt618", ComparisonType.IS_VISIBLE);
+		tg.click("ele_buynowbutt618", 1);
+		tg.close();
+	}
+}
